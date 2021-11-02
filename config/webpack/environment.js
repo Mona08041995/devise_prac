ff4b928e5d7fce957876e7bc1d85dab3c6ca1e7a
Mona08041995 <monika.benke@protonshub.in>
2021-11-02 18:16:08 +0530

    env js chnges

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,10 +3,12 @@ const webpack = require("webpack")
 
 environment.plugins.append("Provide", new webpack.ProvidePlugin({
 
- $: 'jquery/src/jquery',
- jQuery: 'jquery/src/jquery'
+$: 'jquery',
+
+jQuery: 'jquery',
 
 Popper: ['popper.js', 'default']
 
 }))
-module.exports = environment
+
+
