6dbf95ddb197b693fe0b5cc5a4da0dcad34319b3
Mona08041995 <monika.benke@protonshub.in>
2021-11-02 21:09:37 +0530

    chnges

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: "rails_admin/main#dashboard"
 
 end
