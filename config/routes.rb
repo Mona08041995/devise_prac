55b84dc2152b12b60c14e910ed4fd27cf270854c
Mona08041995 <monika.benke@protonshub.in>
2021-11-03 16:09:04 +0530

    chnges

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  mount RailsAdmin::Engine => '/cms', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
    #, :controllers => {:sessions => "users/sessions", :passwords => "users/passwords"}
-
+  devise_scope :user do
+    get '/users/sign_out' => 'users/sessions#destroy'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+   root to: 'home#index'
 end
